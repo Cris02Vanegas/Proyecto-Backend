6f28f1ffdbfb0608ab2149770a26b8829259ddef
Cristhian Vanegas <crisvanegas02@gmail.com>
2024-05-22 20:12:44 -0500

    servidor

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict"
+const express = require("express");
+
+const servidor = express();
+
+servidor.get("/", (solicitud, respuesta) => {
+    respuesta.json({
+        saludo: "Proyecto Backend",
+    })
+});
+
+servidor.get("/xxx", (solicitud, respuesta) => {
+    respuesta.redirect(301, "https://bit.institute/"); // sirve para redireccionar la pg cuando no hay respuesta del enlace
+})
+
+servidor.listen(3000);
+console.log("Servidor corriendo en el puerto 3000");
